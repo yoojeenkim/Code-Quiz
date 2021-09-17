574e1e39fb86b86c67115954cf3e18869518e896
daniel <yoojeenkim@gmail.com>
2021-09-17 15:16:57 -0400

    added basic elements to html, commented out timer in js for now

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,19 @@
-var timer = document.getElementById ('timer');
+// var timer = document.getElementById ('timer');
 
-function countdown() {
-    var timeLeft = 80;
+// function countdown() {
+//     var timeLeft = 80;
 
-    //used a setInterval method to call the function to move in increments of 1000 milliseconds down
-    var timeInterval = setInterval(function() {
-        timeLeft--;
-        timer.textContent = 'Timer: ' + timeLeft;
+//     //used a setInterval method to call the function to move in increments of 1000 milliseconds down
+//     var timeInterval = setInterval(function() {
+//         timeLeft--;
+//         timer.textContent = 'Timer: ' + timeLeft;
 
-        if(timeLeft === 0) {
-            //this will end the game
-            clearInterval(timeInterval);
-            //reset the game back to original
-            //displayYouLostMessage();
-        }
-    }, 1000);
-}
+//         if(timeLeft === 0) {
+//             //this will end the game
+//             clearInterval(timeInterval);
+//             //reset the game back to original
+//             //displayYouLostMessage();
+//         }
+//     }, 1000);
+// }
+
