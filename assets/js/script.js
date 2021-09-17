81c841ad761e39a193e5c3d5934f6c5ff562e090
daniel <yoojeenkim@gmail.com>
2021-09-17 11:10:38 -0400

    added a timer feature

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+var timer = document.getElementById ('timer');
+
+function countdown() {
+    var timeLeft = 80;
+
+    //used a setInterval method to call the function to move in increments of 1000 milliseconds down
+    var timeInterval = setInterval(function() {
+        timeLeft--;
+        timer.textContent = 'Timer: ' + timeLeft;
+
+        if(timeLeft === 0) {
+            //this will end the game
+            clearInterval(timeInterval);
+            //reset the game back to original
+            //displayYouLostMessage();
+        }
+    }, 1000);
+}
