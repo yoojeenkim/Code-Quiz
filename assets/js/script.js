e79054e941c38aa4eb2462c3e27259b508989765
daniel <yoojeenkim@gmail.com>
2021-09-18 00:12:49 -0400

    put all questions and choices into objects, will use functions to call them

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,36 @@
 //     }, 1000);
 // }
 
+var question1 = {
+    //
+}
+
+//Commonly used data types DO NOT include:
+// 1. strings
+// 2. booleans
+// 3. alerts
+// 4. numbers
+// //
+// The condition in an if / else statement is enclosed within _____.
+// 1. quotes
+// 2. curly_brackets
+// 3. parentheses
+// 4. square brackets
+
+// Arrays in JavaScript can be used to store ____.Arrays
+// 1. numbers and strings
+// 2. other arrays
+// 3. booleans
+// 4. all of the above
+// //
+// String values must be enclosed within _____ when being assigned to variables.
+// 1. commas
+// 2. curly brackets
+// 3. quotes
+// 4. parantheses
+
+// A very useful tool used during development and debuggin for print content to the debugger is:
+// 1. JavaScript
+// 2. terminal/bash
+// 3. for loops
+// 4. console.log
