ef9dfb60fa5d7b08b34a6afcd80df658ba46fca6
daniel <yoojeenkim@gmail.com>
2021-09-20 13:52:11 -0400

    reformatted html to allow more styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 console.log(this);
-var header = document.querySelector('.header');
-var body = document.querySelector('.body');
+var quiz = document.getElementById('#quiz');
+var results = document.getElementsByClassName('.results');
 
 var questions = [
     {
@@ -56,40 +56,168 @@ var questions = [
     }
 ];
 
-var questions = [question1, question2, question3, question4, question5];
-
-//the function init will load the title and start button
+//the function init will load on startup of webpage
 function init() {
-    header.textContent = 'Coding Quiz Challenge';
-    header.setAttribute('style', 'text-align:center; background: #9A8194; color: #EBD8B7; border-bottom: 30px #C6A9A3; font-size: 300%');
-    body.textContent = 'Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!';
-    body.setAttribute('style','text-align:center; background: #fff; color: #black; border-bottom: 30px #C6A9A3; font-size:200%');
-    var startButton = document.createElement('button');
-    startButton.appendChild(body);
-    startButton.textContent = 'Start Quiz!';
-    startButton.setAttribute('style','background-color:purple; color:gold');
+    quiz.textContent = 'Coding Quiz Challenge';
 }
 
-function startQuiz(questions,quizContainer, resultsContainer, submitButton) {
+//sample code
+// (function(){
+//   // Functions
+//   function buildQuiz(){
+//     // variable to store the HTML output
+//     const output = [];
 
-    //should i use a for loop?
-        function showQuestion(questions, quizContainer){
-            //code for showing questions
-        }
+//     // for each question...
+//     myQuestions.forEach(
+//       (currentQuestion, questionNumber) => {
 
-        function showResult(questions, quizContainer, resultsContainer){
-            //code for showing the result in the footer
-        }
+//         // variable to store the list of possible answers
+//         const answers = [];
 
-        //show question
-        showQuestion(questions,quizContainer);
+//         // and for each available answer...
+//         for(letter in currentQuestion.answers){
 
-        //when user clicks correct answer, show the result
-        submitButton.onclick = function(){
-            showResult(questions, quizContainer,resultsContainer);
-        }
-    //end of for loop
-}
+//           // ...add an HTML radio button
+//           answers.push(
+//             `<label>
+//               <input type="radio" name="question${questionNumber}" value="${letter}">
+//               ${letter} :
+//               ${currentQuestion.answers[letter]}
+//             </label>`
+//           );
+//         }
+
+//         // add this question and its answers to the output
+//         output.push(
+//           `<div class="slide">
+//             <div class="question"> ${currentQuestion.question} </div>
+//             <div class="answers"> ${answers.join("")} </div>
+//           </div>`
+//         );
+//       }
+//     );
+
+//     // finally combine our output list into one string of HTML and put it on the page
+//     quizContainer.innerHTML = output.join('');
+//   }
+
+//   function showResults(){
+
+//     // gather answer containers from our quiz
+//     const answerContainers = quizContainer.querySelectorAll('.answers');
+
+//     // keep track of user's answers
+//     let numCorrect = 0;
+
+//     // for each question...
+//     myQuestions.forEach( (currentQuestion, questionNumber) => {
+
+//       // find selected answer
+//       const answerContainer = answerContainers[questionNumber];
+//       const selector = `input[name=question${questionNumber}]:checked`;
+//       const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+//       // if answer is correct
+//       if(userAnswer === currentQuestion.correctAnswer){
+//         // add to the number of correct answers
+//         numCorrect++;
+
+//         // color the answers green
+//         answerContainers[questionNumber].style.color = 'lightgreen';
+//       }
+//       // if answer is wrong or blank
+//       else{
+//         // color the answers red
+//         answerContainers[questionNumber].style.color = 'red';
+//       }
+//     });
+
+//     // show number of correct answers out of total
+//     resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+//   }
+
+//   function showSlide(n) {
+//     slides[currentSlide].classList.remove('active-slide');
+//     slides[n].classList.add('active-slide');
+//     currentSlide = n;
+//     if(currentSlide === 0){
+//       previousButton.style.display = 'none';
+//     }
+//     else{
+//       previousButton.style.display = 'inline-block';
+//     }
+//     if(currentSlide === slides.length-1){
+//       nextButton.style.display = 'none';
+//       submitButton.style.display = 'inline-block';
+//     }
+//     else{
+//       nextButton.style.display = 'inline-block';
+//       submitButton.style.display = 'none';
+//     }
+//   }
+
+//   function showNextSlide() {
+//     showSlide(currentSlide + 1);
+//   }
+
+//   function showPreviousSlide() {
+//     showSlide(currentSlide - 1);
+//   }
+
+//   // Variables
+//   const quizContainer = document.getElementById('quiz');
+//   const resultsContainer = document.getElementById('results');
+//   const submitButton = document.getElementById('submit');
+//   const myQuestions = [
+//     {
+//       question: "Who invented JavaScript?",
+//       answers: {
+//         a: "Douglas Crockford",
+//         b: "Sheryl Sandberg",
+//         c: "Brendan Eich"
+//       },
+//       correctAnswer: "c"
+//     },
+//     {
+//       question: "Which one of these is a JavaScript package manager?",
+//       answers: {
+//         a: "Node.js",
+//         b: "TypeScript",
+//         c: "npm"
+//       },
+//       correctAnswer: "c"
+//     },
+//     {
+//       question: "Which tool can you use to ensure code quality?",
+//       answers: {
+//         a: "Angular",
+//         b: "jQuery",
+//         c: "RequireJS",
+//         d: "ESLint"
+//       },
+//       correctAnswer: "d"
+//     }
+//   ];
+
+//   // Kick things off
+//   buildQuiz();
+
+//   // Pagination
+//   const previousButton = document.getElementById("previous");
+//   const nextButton = document.getElementById("next");
+//   const slides = document.querySelectorAll(".slide");
+//   let currentSlide = 0;
+
+//   // Show the first slide
+//   showSlide(currentSlide);
+
+//   // Event listeners
+//   submitButton.addEventListener('click', showResults);
+//   previousButton.addEventListener("click", showPreviousSlide);
+//   nextButton.addEventListener("click", showNextSlide);
+// })();
+
 // var timer = document.getElementById ('timer');
 
 // function countdown() {
