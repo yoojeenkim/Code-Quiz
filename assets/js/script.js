b3e9bf8e9838f4ab847bcfcb70e32144eff532a6
daniel <yoojeenkim@gmail.com>
2021-09-21 20:11:44 -0400

    working quiz, moves from slide to slide, added base html and js for results page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,19 @@
 var container = document.querySelector('.container');
 var startButton = document.querySelector('#start-button');
 var quizContainer = document.querySelector('#quiz-container');
+var quizQuestion = document.querySelector('#question');
+var choice1 = document.querySelector('.choice-text1');
+var choice2 = document.querySelector('.choice-text2');
+var choice3 = document.querySelector('.choice-text3');
+var choice4 = document.querySelector('.choice-text4');
+var choiceButton1 = document.querySelector('.choice-button1');
+var choiceButton2 = document.querySelector('.choice-button2');
+var choiceButton3 = document.querySelector('.choice-button3');
+var choiceButton4 = document.querySelector('.choice-button4');
+var results = document.querySelector('.results');
+var finalScore = document.querySelector('#final-score-container');
+
+var i = 0;
 
 var questions = [
     {
@@ -18,7 +31,7 @@ var questions = [
         choices: {
             choice1: 'quotes',
             choice2: 'curly_brackets',
-            chocie3: 'parantheses',
+            choice3: 'parantheses',
             choice4: 'square brackets'
         },
         correctAnswer: 'choice3'
@@ -56,7 +69,77 @@ var questions = [
     }
 ];
 
+function showResults () {
+    questions.textContent = 'All done!';
+    quizContainer.style.display = 'none';
+    finalScore.style.display = 'flex';
+}
+
+function checkAnswer1 () {
+    if (questions[i].correctAnswer == 'choice1') {
+        results.textContent = 'Correct!';
+    } else {
+        results.textContent = 'Wrong!'
+    }
+    i++;
+    generateNewQuestion();
+}
+
+function checkAnswer2 () {
+    if (questions[i].correctAnswer == 'choice2') {
+        results.textContent = 'Correct!';
+    }  else {
+        results.textContent = 'Wrong!';
+    }
+    i++;
+    generateNewQuestion();
+}
+
+function checkAnswer3 () {
+    if (questions[i].correctAnswer == 'choice3') {
+        results.textContent = 'Correct!';
+    }  else {
+        results.textContent = 'Wrong!';
+    }
+    i++;
+    generateNewQuestion();
+}
+
+function checkAnswer4 () {
+    if (questions[i].correctAnswer == 'choice4') {
+        results.textContent = 'Correct!';
+    }  else {
+        results.textContent = 'Wrong!';
+    }
+
+    i++;
+    generateNewQuestion();
+}
+
+function generateNewQuestion () {
+    if(i < questions.length) {
+        quizQuestion.textContent = questions[i].question;
+        choice1.textContent = questions[i].choices.choice1;
+        choice2.textContent = questions[i].choices.choice2;
+        choice3.textContent = questions[i].choices.choice3;
+        choice4.textContent = questions[i].choices.choice4;
+        
+        choiceButton1.addEventListener('click', checkAnswer1);
+        choiceButton2.addEventListener('click', checkAnswer2);
+        choiceButton3.addEventListener('click', checkAnswer3);
+        choiceButton4.addEventListener('click', checkAnswer4);
+    } else if (i === questions.length) {
+        showResults();
+    }
+
+}
+
+function startQuiz () {
+    generateNewQuestion(); 
+}
+
 startButton.addEventListener('click', function() {
   container.style.display = 'none';
   quizContainer.style.display = 'flex';
+  startQuiz();
 })
